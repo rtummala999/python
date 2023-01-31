7e522e21876c9658e507a9c97cfc5e63f85e52ff
RAMGOPAL TUMMALA <121357680+rtummala999@users.noreply.github.com>
2023-01-31 15:19:17 -0800

    Create class.py

---
+++ oops/class.py
@@ -0,0 +1,12 @@
+class Student:
+
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name
+    def print(self):
+        print(self.name,self.id)
+
+
+ram=Student(18003085,"Ramgopal tummala") 
+
+ram.print()
