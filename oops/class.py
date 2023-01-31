43209ceff997307763bdce817c3c247c42b57707
RAMGOPAL TUMMALA <121357680+rtummala999@users.noreply.github.com>
2023-01-31 15:22:03 -0800

    Update class.py

--- oops/class.py
+++ oops/class.py
@@ -1,12 +1,17 @@
 class Student:
-
-    def __init__(self,id,name):
+#class main
+    def __init__(self,id,name): #default constrct 
         self.id=id
         self.name=name
-    def print(self):
+    def print(self):            #method 
         print(self.name,self.id)
 
+        # here int the print method we are calling the class.name and class.id 
+
+#self is used to point within class details 
+# self.name is refering to class.name 
 
 ram=Student(18003085,"Ramgopal tummala") 
-
+#creating objcts to the class
 ram.print()
+#calling the print method 
